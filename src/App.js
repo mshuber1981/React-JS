e8ef154cdf52da7977dcdd5a276a8d1652c6d843
Michael Huber <michael.huber1981@outlook.com>
2021-11-25 11:16:22 -0600

    Adding light/dark theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+import { useState, useEffect } from "react";
+import styled, { ThemeProvider } from "styled-components";
 import GlobalStyles from "./GlobalStyles";
 
+const darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+const themes = {
+  light: {
+    color: "hsl(209, 61%, 16%)",
+    background: "hsl(210, 36%, 96%)",
+  },
+  dark: {
+    color: "hsl(210, 36%, 96%)",
+    background: "hsl(209, 61%, 16%)",
+  },
+};
+
 export default function App() {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(function () {
+    if (darkMode) {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  }, []);
+
   return (
-    <div>
-      <GlobalStyles />
-      <h1>React</h1>
-    </div>
+    <>
+      <ThemeProvider theme={themes[theme]}>
+        <GlobalStyles />
+        <h1>React</h1>
+      </ThemeProvider>
+    </>
   );
 }

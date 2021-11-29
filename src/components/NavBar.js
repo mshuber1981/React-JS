af477241c34509a3d36f91d2f4fcc271a1ac0242
Michael Huber <michael.huber1981@outlook.com>
2021-11-29 09:47:31 -0600

    Adding NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+import { FaReact, FaBars } from "react-icons/fa";
+
+const StyledNavBar = styled.nav`
+  .nav-center {
+    max-width: var(--max-width);
+    margin: 0 auto;
+    padding: 0.25rem 0;
+
+    .nav-header {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      align-items: center;
+      font-size: 2rem;
+      margin: 0 0.25rem;
+
+      .nav-btn {
+        line-height: 0;
+        background: transparent;
+        border-color: transparent;
+        color: ${(props) => props.theme.color};
+        cursor: pointer;
+        justify-self: end;
+        font-size: 2rem;
+      }
+    }
+  }
+`;
+
+export default function NavBar() {
+  return (
+    <StyledNavBar>
+      <div className="nav-center">
+        <div className="nav-header">
+          <FaReact />
+          <button type="button" className="nav-btn">
+            <FaBars />
+          </button>
+        </div>
+      </div>
+    </StyledNavBar>
+  );
+}

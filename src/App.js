9a6bbccf37c0f7d9c402804771cae7afb872c228
Michael Huber <michael.huber1981@outlook.com>
2021-11-29 09:49:18 -0600

    Adding NavBar and Switch/toggle theme components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { useState, useEffect } from "react";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./GlobalStyles";
+import NavBar from "./components/NavBar";
+import Switch from "./components/Switch";
 
 const darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
 const themes = {
   light: {
+    name: "light",
     color: "hsl(209, 61%, 16%)",
     background: "hsl(210, 36%, 96%)",
   },
   dark: {
+    name: "dark",
     color: "hsl(210, 36%, 96%)",
     background: "hsl(209, 61%, 16%)",
   },
@@ -17,6 +21,10 @@ const themes = {
 
 export default function App() {
   const [theme, setTheme] = useState("light");
+
+  function toggleTheme() {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  }
 
   useEffect(function () {
     if (darkMode) {
@@ -30,7 +38,11 @@ export default function App() {
     <>
       <ThemeProvider theme={themes[theme]}>
         <GlobalStyles />
-        <h1>React</h1>
+        <NavBar />
+        <main>
+          <h1>React</h1>
+          <Switch theme={theme} toggleTheme={toggleTheme} />
+        </main>
       </ThemeProvider>
     </>
   );

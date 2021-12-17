5574e7a9fdbfa0bef4e6a9a0f2ac5bb8cb83af4f
Michael Huber <michael.huber1981@outlook.com>
2021-12-17 16:03:45 -0600

    Added InnerButton, removed redundant styling

--- src/components/Menu/Categories.jsx
+++ src/components/Menu/Categories.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { InnerButton } from "../styledComponents";
 
 const StyledCategories = styled.div`
   margin: 3rem 0;
@@ -7,35 +8,7 @@ const StyledCategories = styled.div`
   justify-content: center;
 
   .filter-btn {
-    font-size: 1rem;
-    text-transform: capitalize;
     margin: 0.5rem 0.5rem;
-    letter-spacing: 1px;
-    padding: 0.375rem 0.75rem;
-    color: var(--clr-gold);
-    cursor: pointer;
-    transition: var(--transition);
-    border-radius: var(--radius);
-    border: 2px solid var(--clr-primary-5);
-    background: ${(props) =>
-      props.theme.name === "light"
-        ? "var(--clr-grey-1)"
-        : "var(--clr-grey-10)"};
-    color: ${(props) =>
-      props.theme.name === "light"
-        ? "var(--clr-grey-10)"
-        : "var(--clr-grey-1)"};
-
-    &:hover {
-      color: ${(props) =>
-        props.theme.name === "light"
-          ? "var(--clr-grey-1)"
-          : "var(--clr-grey-10)"};
-      background: ${(props) =>
-        props.theme.name === "light"
-          ? "var(--clr-grey-10)"
-          : "var(--clr-grey-1)"};
-    }
   }
 `;
 
@@ -44,14 +17,13 @@ export default function Categories({ categories, filterItems }) {
     <StyledCategories>
       {categories.map((category, index) => {
         return (
-          <button
-            type="button"
+          <InnerButton
             className="filter-btn"
             key={index}
             onClick={() => filterItems(category)}
           >
             {category}
-          </button>
+          </InnerButton>
         );
       })}
     </StyledCategories>
